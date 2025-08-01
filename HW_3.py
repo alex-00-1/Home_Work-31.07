c3745f930790e22c8e87e9684365ef3dd6380326
Alexl <zykoleksandr@gmail.com>
2025-08-01 21:15:12 +0300

    Calculator

---
+++ HW_3.py
@@ -0,0 +1,21 @@
+
+
+num1, num2 = int(input("Ведіть перше число: ")), int(input("Введіть друге число: "))
+sign = input("Введіть арефметичний  знак: ")
+
+if sign == "+":
+    print(f'{num1} + {num2} = {num1 + num2}')
+
+if sign == "-":
+    print(f'{num1} - {num2} = {num1 - num2}')
+
+if sign == "*":
+    print(f'{num1} * {num2} = {num1 * num2}')
+
+if sign == "/":
+    if num2 == 0:
+        print("На нуль ділит не можна !!!")
+        num2 = int(input("Змініть значення для другого числа: "))
+        print(f'{num1} / {num2} = {num1 / num2}')
+    else:
+        print(f'{num1} / {num2} = {num1 / num2}')
